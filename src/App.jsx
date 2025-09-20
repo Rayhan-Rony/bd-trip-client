345be40fd3036cfcecbfd5f8996b3774f219d9aa
Rayhan-Rony <jobayerahmedrony@gmail.com>
2025-09-21 03:29:06 +0600

    added basic structure with react router

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center h-screen">
+        <h1>welcome</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/routers/router.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../layouts/mainLayout";
+import Home from "../pages/Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+    ],
+  },
+]);
+export default router;
